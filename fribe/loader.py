ec4825619c5a225ffd03ac8c535f68329ee8757d
Imre Piller <piller.imre@gmail.com>
2017-11-28 14:58:11 +0100

    Corrected paths for grammars

--- fribe/loader.py
+++ fribe/loader.py
@@ -6,6 +6,8 @@ from fribe.tokenizer import Tokenizer
 from fribe.parser import TokenClassifier
 from fribe.parser import Parser
 
+import os
+
 
 def load_engine_from_string(source):
     """
@@ -13,12 +15,16 @@ def load_engine_from_string(source):
     :param source: the source text of the rulebase
     :return: an engine object
     """
+    package_directory = os.path.dirname(os.path.abspath(__file__))
     char_source = SourceString(source)
     char_classifier = CharClassifier()
-    tokenizer_grammar = Grammar(filename='../grammars/simple/tokenizer.grammar', classifier=char_classifier)
+    tokenizer_path = os.path.join(package_directory, '../grammars/simple/tokenizer.grammar')
+    tokenizer_grammar = Grammar(filename=tokenizer_path, classifier=char_classifier)
     tokenizer = Tokenizer(tokenizer_grammar, char_source)
     token_classifier = TokenClassifier()
-    parser_grammar = Grammar(filename='../grammars/simple/parser.grammar', classifier=token_classifier)
+    parser_path = os.path.join(package_directory, '../grammars/simple/parser.grammar')
+    parser_grammar = Grammar(filename=parser_path, classifier=token_classifier)
     parser = Parser(parser_grammar, tokenizer)
     parser.parse()
     return parser.engine
+
