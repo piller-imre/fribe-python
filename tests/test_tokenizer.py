a389f9bf5a517db440602d70fb123a07f961182b
Imre Piller <piller.imre@gmail.com>
2021-10-31 10:26:45 +0100

    Exprail related test cases have been adapted to new version

--- tests/test_tokenizer.py
+++ tests/test_tokenizer.py
@@ -11,8 +11,8 @@ class TokenizerTest(unittest.TestCase):
     """Tests for the description language tokenizer"""
 
     def test_empty_stream(self):
-        char_classifier = CharClassifier()
-        grammar = Grammar(filename='../grammars/simple/tokenizer.grammar', classifier=char_classifier)
+        char_classifier = CharClassifier.is_in_class
+        grammar = Grammar(filename='grammars/simple/tokenizer.grammar', classifier=char_classifier)
         source = SourceString('')
         parser = Tokenizer(grammar, source)
         parser.parse()
@@ -21,8 +21,8 @@ class TokenizerTest(unittest.TestCase):
         self.assertEqual('', token.value)
 
     def test_whitespace_only_stream(self):
-        char_classifier = CharClassifier()
-        grammar = Grammar(filename='../grammars/simple/tokenizer.grammar', classifier=char_classifier)
+        char_classifier = CharClassifier.is_in_class
+        grammar = Grammar(filename='grammars/simple/tokenizer.grammar', classifier=char_classifier)
         source = SourceString('      ')
         parser = Tokenizer(grammar, source)
         parser.parse()
@@ -31,8 +31,8 @@ class TokenizerTest(unittest.TestCase):
         self.assertEqual('', token.value)
 
     def test_single_keyword(self):
-        char_classifier = CharClassifier()
-        grammar = Grammar(filename='../grammars/simple/tokenizer.grammar', classifier=char_classifier)
+        char_classifier = CharClassifier.is_in_class
+        grammar = Grammar(filename='grammars/simple/tokenizer.grammar', classifier=char_classifier)
         source = SourceString('universe')
         parser = Tokenizer(grammar, source)
         parser.parse()
@@ -41,8 +41,8 @@ class TokenizerTest(unittest.TestCase):
         self.assertEqual('universe', token.value)
 
     def test_single_keyword_with_padding(self):
-        char_classifier = CharClassifier()
-        grammar = Grammar(filename='../grammars/simple/tokenizer.grammar', classifier=char_classifier)
+        char_classifier = CharClassifier.is_in_class
+        grammar = Grammar(filename='grammars/simple/tokenizer.grammar', classifier=char_classifier)
         source = SourceString('     universe     ')
         parser = Tokenizer(grammar, source)
         parser.parse()
@@ -51,8 +51,8 @@ class TokenizerTest(unittest.TestCase):
         self.assertEqual('universe', token.value)
 
     def test_multiple_keywords(self):
-        char_classifier = CharClassifier()
-        grammar = Grammar(filename='../grammars/simple/tokenizer.grammar', classifier=char_classifier)
+        char_classifier = CharClassifier.is_in_class
+        grammar = Grammar(filename='grammars/simple/tokenizer.grammar', classifier=char_classifier)
         source = SourceString('universe description rule when and is end')
         parser = Tokenizer(grammar, source)
         keywords = ['universe', 'description', 'rule', 'when', 'and', 'is', 'end']
@@ -63,8 +63,8 @@ class TokenizerTest(unittest.TestCase):
             self.assertEqual(keywords.pop(0), token.value)
 
     def test_single_text(self):
-        char_classifier = CharClassifier()
-        grammar = Grammar(filename='../grammars/simple/tokenizer.grammar', classifier=char_classifier)
+        char_classifier = CharClassifier.is_in_class
+        grammar = Grammar(filename='grammars/simple/tokenizer.grammar', classifier=char_classifier)
         source = SourceString('"single text"')
         parser = Tokenizer(grammar, source)
         parser.parse()
@@ -73,8 +73,8 @@ class TokenizerTest(unittest.TestCase):
         self.assertEqual('single text', token.value)
 
     def test_multiple_texts(self):
-        char_classifier = CharClassifier()
-        grammar = Grammar(filename='../grammars/simple/tokenizer.grammar', classifier=char_classifier)
+        char_classifier = CharClassifier.is_in_class
+        grammar = Grammar(filename='grammars/simple/tokenizer.grammar', classifier=char_classifier)
         source = SourceString('"first"  "second"\n\n"third"')
         parser = Tokenizer(grammar, source)
         texts = ['first', 'second', 'third']
@@ -85,8 +85,8 @@ class TokenizerTest(unittest.TestCase):
             self.assertEqual(texts.pop(0), token.value)
 
     def test_quoted_texts(self):
-        char_classifier = CharClassifier()
-        grammar = Grammar(filename='../grammars/simple/tokenizer.grammar', classifier=char_classifier)
+        char_classifier = CharClassifier.is_in_class
+        grammar = Grammar(filename='grammars/simple/tokenizer.grammar', classifier=char_classifier)
         source = SourceString('"\\"first\\""  "sec\\\\ond"\n\n"th\\\\\\"\\\\rd"')
         parser = Tokenizer(grammar, source)
         texts = ['"first"', 'sec\\ond', 'th\\"\\rd']
@@ -97,8 +97,8 @@ class TokenizerTest(unittest.TestCase):
             self.assertEqual(texts.pop(0), token.value)
 
     def test_single_number(self):
-        char_classifier = CharClassifier()
-        grammar = Grammar(filename='../grammars/simple/tokenizer.grammar', classifier=char_classifier)
+        char_classifier = CharClassifier.is_in_class
+        grammar = Grammar(filename='grammars/simple/tokenizer.grammar', classifier=char_classifier)
         source = SourceString('1234')
         parser = Tokenizer(grammar, source)
         parser.parse()
@@ -107,8 +107,8 @@ class TokenizerTest(unittest.TestCase):
         self.assertEqual('1234', token.value)
 
     def test_multiple_integers(self):
-        char_classifier = CharClassifier()
-        grammar = Grammar(filename='../grammars/simple/tokenizer.grammar', classifier=char_classifier)
+        char_classifier = CharClassifier.is_in_class
+        grammar = Grammar(filename='grammars/simple/tokenizer.grammar', classifier=char_classifier)
         source = SourceString(' 12 34 \n    -567   \n\n-8\n \n')
         parser = Tokenizer(grammar, source)
         numbers = ['12', '34', '-567', '-8']
@@ -119,8 +119,8 @@ class TokenizerTest(unittest.TestCase):
             self.assertEqual(numbers.pop(0), token.value)
 
     def test_multiple_floats(self):
-        char_classifier = CharClassifier()
-        grammar = Grammar(filename='../grammars/simple/tokenizer.grammar', classifier=char_classifier)
+        char_classifier = CharClassifier.is_in_class
+        grammar = Grammar(filename='grammars/simple/tokenizer.grammar', classifier=char_classifier)
         source = SourceString('.101, 10.20,\n\n -8.9  -7.6  -.888')
         parser = Tokenizer(grammar, source)
         numbers = ['.101', '10.20', '-8.9', '-7.6', '-.888']
@@ -131,8 +131,8 @@ class TokenizerTest(unittest.TestCase):
             self.assertEqual(numbers.pop(0), token.value)
 
     def test_finish_token(self):
-        char_classifier = CharClassifier()
-        grammar = Grammar(filename='../grammars/simple/tokenizer.grammar', classifier=char_classifier)
+        char_classifier = CharClassifier.is_in_class
+        grammar = Grammar(filename='grammars/simple/tokenizer.grammar', classifier=char_classifier)
         source = SourceString('end')
         parser = Tokenizer(grammar, source)
         parser.parse()
